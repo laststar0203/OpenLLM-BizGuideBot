9404cb61fa18ef22475d22234b95bdb4ea6df973
laststar0203 <laststar0203@gmail.com>
2024-11-15 14:07:32 +0000

    [FIX] Update document path

--- source/rag_server/main.py
+++ source/rag_server/main.py
@@ -4,8 +4,11 @@ from app.chat_app import ChatApp
 def main():
 
     current_dir = os.getcwd()
-    document_dir = os.path.abspath(os.path.join(current_dir, "../../document"))
-    
+    if current_dir.startswith("/home"):
+        document_dir = os.path.abspath(os.path.join(current_dir, "../../document"))
+    else:
+        document_dir = "/mount/src/rag-service/document"
+            
     app = ChatApp({
         "llm_url": "https://incredibly-mature-vulture.ngrok-free.app/llm/",
         "document_dir": document_dir,
