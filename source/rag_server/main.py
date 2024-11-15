38b1f1a401ce3c2bec0fd3cb36a3faf6a601afbe
laststar0203 <laststar0203@gmail.com>
2024-11-15 13:56:56 +0000

    [FIX] Update document path

--- source/rag_server/main.py
+++ source/rag_server/main.py
@@ -1,9 +1,14 @@
+import os
 from app.chat_app import ChatApp
 
 def main():
+
+    current_dir = os.getcwd()
+    document_dir = os.path.abspath(os.path.join(current_dir, "../../document"))
+    
     app = ChatApp({
         "llm_url": "https://incredibly-mature-vulture.ngrok-free.app/llm/",
-        "document_dir": "../../document",
+        "document_dir": document_dir,
         "show_prompt" : True,
     })
     app.run()
